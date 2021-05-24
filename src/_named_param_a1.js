ec27397db8f887b06be228b4fd84e8794f05df4b
Brian Takita <brian.takita@gmail.com>
2021-05-24 19:54:43 -0400

    ## 6.2.1
    ### Patch Changes
    
    - .gitignore: - \*.js
    - Updated dependencies [undefined]
    - @ctx-core/array@20.4.1

---
+++ src/_named_param_a1.js
@@ -0,0 +1,5 @@
+import { map } from '@ctx-core/array';
+export function _named_param_a1(param_a1) {
+    return (map(param_a1, (_, i) => `$${i + 1}`));
+}
+export { _named_param_a1 as _a1__named_param };
