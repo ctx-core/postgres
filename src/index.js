b2f2cc3385ab7cf4fd60158e3431b3f139ec9b4d
Brian Takita <brian.takita@gmail.com>
2021-06-18 04:09:33 -0400

    rm src/*.js

--- src/index.js
+++
@@ -1,4 +0,0 @@
-export * from './named_param_a_';
-export * from './param_a_set_sql_';
-export * from './sadd_named_param';
-//# sourceMappingURL=src/index.js.map
--- src/param_a_set_sql_.js
+++
@@ -1,6 +0,0 @@
-import { map } from '@ctx-core/array';
-export function param_a_set_sql_(param_a, named_param_a) {
-    return (map(param_a, (param, i) => `${param}=${named_param_a[i]}`).join(', '));
-}
-export { param_a_set_sql_ as _sql__set__a1__param };
-//# sourceMappingURL=src/param_a_set_sql_.js.map
